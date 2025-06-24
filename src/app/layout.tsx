f354c7e1040396bf5d2c00d51bd27bb5d019d9c5
sharekim93 <sksyag@naver.com>
2025-06-24 23:04:19 +0900

    refactor: og:image png-> webp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
     title: "삼성영어 셀레나 아이린 석성 교습소",
     description:
       "삼성영어 셀레나 아이린 석성 교습소입니다. 어은목 마을에 위치해 있고 석성초, 동백동 동백역 셀레나 영어학원입니다.",
-    images: [{ url: "/logo.png" }],
+    images: [{ url: "/logo.webp" }],
     url: "https://www.selena-english.com",
     type: "website",
   },
@@ -52,7 +52,7 @@ export const metadata: Metadata = {
     title: "삼성영어 셀레나 아이린 석성 교습소",
     description:
       "삼성영어 셀레나 아이린 석성 교습소입니다. 어은목 마을에 위치해 있고 석성초, 동백동 동백역 셀레나 영어학원입니다.",
-    images: [{ url: "/logo.png" }],
+    images: [{ url: "/logo.webp" }],
   },
 };
 
